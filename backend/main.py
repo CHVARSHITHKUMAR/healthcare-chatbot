0f218aa6dd09f133788bcaff10e224648b063327
CHINTHA VARSHITH KUMAR <2310030075@klh.edu.in>
2026-03-05 22:19:48 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,8 @@ SECRET_KEY = "supersecretkey"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
-DATABASE_URL = "sqlite:///./medical_history.db"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATABASE_URL = f"sqlite:///{os.path.join(BASE_DIR, 'medical_history.db')}"
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
